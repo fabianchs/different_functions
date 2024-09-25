637ee5d90b789ba2336690c3900efc5ec1b85b85
chackymeow <fabichasola@hotmail.com>
2024-09-25 15:06:03 +0000

    done

--- GetTolerance.py
+++ GetTolerance.py
@@ -1,4 +1,3 @@
-
 def GetTolerance(thickness,concentricity):
 
     upper_thick, lower_thick = thickness, thickness
@@ -9,6 +8,6 @@ def GetTolerance(thickness,concentricity):
         lower_thick-=1
         percentage=int((lower_thick/upper_thick)*100)
 
-    return("Lower thickness:",lower_thick,"Upper thickness:",upper_thick, "Concentricity:",round((lower_thick/upper_thick)*100,2))
+    return("Lower thickness:",lower_thick,"Upper thickness:",upper_thick, "Concentricity:",round((lower_thick/upper_thick)*100,2),"Tolerance:", upper_thick-lower_thick)
 
 print(GetTolerance(1465,85))
